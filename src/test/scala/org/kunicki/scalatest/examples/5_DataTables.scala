5ac0cb5703548403a8bd064109a0b9e4e88d278e
Jacek Kunicki <jacek.kunicki@softwaremill.com>
2019-10-16 23:54:10 +0200

    Yet another approach to data tables

--- src/test/scala/org/kunicki/scalatest/examples/5_DataTables.scala
+++ src/test/scala/org/kunicki/scalatest/examples/5_DataTables.scala
@@ -18,6 +18,21 @@ class DataSets extends BaseSpec {
   }
 }
 
+class SeparateTests extends BaseSpec {
+
+  private val data = List(
+    (2, 2, 4),
+    (2, 0, 3),
+    (2, 3, 5)
+  )
+
+  data.foreach { case (a, b, c) =>
+    it should s"compute $a + $b = $c" in {
+      a + b shouldBe c
+    }
+  }
+}
+
 class DataTable extends BaseSpec with TableDrivenPropertyChecks {
 
   private val dataTable = Table(
